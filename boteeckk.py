d62693515dee815ccf7ab60974ebf7ca57f34ac4
zxcprint <777@yunin.pro>
2025-10-02 18:16:31 +0700

    Update boteeckk.py

--- boteeckk.py
+++ boteeckk.py
@@ -273,3 +273,18 @@ def main():
 if __name__ == "__main__":
     main()
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
+
